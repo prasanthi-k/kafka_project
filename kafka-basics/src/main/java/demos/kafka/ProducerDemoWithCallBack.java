bb0f3612f41bf78fa681c47f9879b8095f484788
prasanthi-k <kalluriprasanthi99@gmail.com>
2022-05-04 10:15:48 +0530

    to add a sticky partioner with callback

---
+++ kafka-basics/src/main/java/demos/kafka/ProducerDemoWithCallBack.java
@@ -0,0 +1,57 @@
+package demos.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallBack {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallBack.class);
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer");
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //to see working ok a sticky partitioner which means the producer that receives messages sent in time close to each other
+        // will try to fill a batch into ONE partition before switching to creating a batch for another partition
+        for (int i=0; i<10; i++ ) {
+            // create a producer record
+            ProducerRecord<String, String> producerRecord =
+                    new ProducerRecord<String, String>("demo_java", "hello world " + Integer.toString(i));
+
+            // send data - asynchronous
+            producer.send(producerRecord, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        // the record was successfully sent
+                        log.info("Received new metadata. \n" +
+                                "Topic:" + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        log.error("Error while producing", e);
+                    }
+                }
+            });
+        }
+
+        // flush data - synchronous
+        producer.flush();
+        // flush and close producer
+        producer.close();
+    }
+}
